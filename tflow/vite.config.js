b8ac44dda1430b58dbe9c3f98380ca17b673b193
Cleevesh <ibukunoluwafemi18@gmail.com>
2024-09-22 19:19:34 +0100

    test 1

--- tflow/vite.config.js
+++ tflow/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        popup: "index.html", // Main popup HTML file
+        background: "src/background/background.js", // Path to the background script
+      },
+      output: {
+        entryFileNames: "[name].bundle.js", // Name the output files
+      },
+    },
+  },
+});
